8bc2c559fb7cac1dfaac8443a863fc352577e55b
Christopher E. Siu <cesiu@calpoly.edu>
2025-02-19 16:02:22 -0800

    Added pseucodocode for removing and traversing BSTs.

--- 02.19-bst-traversals/bst.py
+++ 02.19-bst-traversals/bst.py
@@ -21,12 +21,25 @@ class Node:
 
 
 def find(bst, key):
+    # Return the result of finding the given key at the given bst's root.
+    pass
+
+
+def _find(node, key):
+    # If the given node is None, then:
+    #     (the given key does not exist)
+    # Else if the given key is equal to the given node's key, then:
+    #     (the given node contains the given key)
+    # Else if the given key is less than the given node's key, then:
+    #     Return the result of recursing on the given node's left.
+    # Else, do:
+    #     Return the result of recursing on the given node's right.
     pass
 
 
 def insert(bst, key):
-    # Set the given bst's root to the result of inserting the given key at the
-    #  given bst's root.
+    # Set the given bst's root to the result of inserting the given key into
+    #  the given bst's root.
     pass
 
 
@@ -51,8 +64,66 @@ def _insert(bst, node, key):
 
 
 def remove(bst, key):
+    # Set the given bst's root to the result of removing the given key from
+    #  the given bst's root.
+    pass
+
+
+def _remove(bst, node, key):
+    # If the given node is None, then:
+    #     (the given key does not exist within the given BST -- do whatever
+    #      ought to be done with non-existent keys, e.g., raise an error)
+    # Else if the given key is equal to the given node's key, then:
+    #     If the given node's right is None, then:
+    #         Decrement the size.
+    #         Return the given node's left.
+    #     Else if the given node's left is None, then:
+    #         Decrement the size.
+    #         Return the given node's right.
+    #     Else, do:
+    #         NOTE: Consider the smallest key in the right subtree:
+    #                 * By definition, it's larger than any to the left.
+    #                 * By construction, it's smaller than any to the right.
+    #               ...thus, that key can *replace* the given node's key.
+    #
+    #         Find the smallest key in the given node's right.
+    #         Set the given node's right to the result of recursively removing
+    #          that smallest key from the given node's right.
+    #         Set the given node's key to that smallest key.
+    #
+    # Else if the given key is less than the given node's key, then:
+    #     Set the given node's left to the result of recursively removing the
+    #      given key from the given node's left.
+    #     Return the given node.
+    # Else, do:
+    #     Set the given node's right to the result of recursively removing the
+    #      given key from the given node's right.
+    #     Return the given node.
+    pass
+
+
+def _min(node):
+    # If the given node is None, then:
+    #     (there is no smallest key)
+    # Else if the given node's left is None, then:
+    #     Return the given node's key.
+    # Else, do:
+    #     Return the result of recursing on the given node's left.
     pass
 
 
 def inorder(bst):
+    # Traverse the given tree's root.
+    pass
+
+
+def _traverse(node):
+    # If the given node is None, then:
+    #     (there is no node to traverse -- perform whatever default operation
+    #      should be performed on empty trees)
+    # Else, do:
+    #     Recursively traverse the given node's left.
+    #     (traverse the given node -- perform whatever operation should be
+    #      performed on each node in the tree, e.g., enqueue it to a queue)
+    #     Recursively traverse the given node's right.
     pass
