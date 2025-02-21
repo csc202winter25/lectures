f9e5ff79dd014f04e251d71b1ec1fbe82bf5c7a5
Christopher Siu <christopher@siu.sh>
2025-02-21 15:56:52 -0800

    Added notes on implementing priority queues as binary heaps.

--- 02.21-priority-queues/priority_queue.py
+++ 02.21-priority-queues/priority_queue.py
@@ -11,12 +11,27 @@ class PriorityQueue:
 
 
 def enqueue(pqueue, value):
+    # TODO: Add the value as a new node in the tree. That node must be either
+    #       the next leaf in the lowest (partially filled) level or the first
+    #       leaf in a new lowest (if the old lowest was full) level, so as to
+    #       maintain the completeness of the tree. Then rearrange nodes as
+    #       necesssary in order to maintain the heap property.
     pass
 
 
 def dequeue(pqueue):
+    # TODO: Remove the root and replace it with the last leaf in the lowest
+    #       level, so as to maintain both the structural integrity and the
+    #       completeness of the tree. Then rearrange nodes as necessary in
+    #       order to maintain the heap property.
     pass
 
 
 def peek(pqueue):
+    # NOTE: The largest value must always be at the root (the second largest
+    #       must be one of the root's children, but there is no way of knowing
+    #       which one; the smallest must be one of the leaves, but there is no
+    #       way of knowing which one...).
+    #
+    # (return the value at the root)
     pass
