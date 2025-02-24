85b175c3f8a6eee6c9e05e38f994d153991e6395
Christopher E. Siu <cesiu@calpoly.edu>
2025-02-24 12:04:41 -0800

    Added pseudocode for binary heaps.

--- 02.24-heaps/priority_queue.py
+++ 02.24-heaps/priority_queue.py
@@ -15,7 +15,26 @@ def enqueue(pqueue, value):
     #       the next leaf in the lowest (partially filled) level or the first
     #       leaf in a new lowest (if the old lowest was full) level, so as to
     #       maintain the completeness of the tree. Then rearrange nodes as
-    #       necesssary in order to maintain the heap property.
+    #       necesssary in order to maintain the heap property: repeatedly swap
+    #       the current node with its parent if the current node is larger.
+    #
+    # If the size is equal to the capacity, then:
+    #     Set the capacity to capacity * 2.
+    #     Create a new array of that capacity + 1.
+    #     (copy the elements from the old array into the new array)
+    #     Set the array to the new array.
+    #
+    # Start with index i being the index of the next leaf (index size + 1).
+    # Set element i to the given value.
+    #
+    # While i is greater than 1 (i is not the root), do:
+    #     If element i is greater than element i // 2, then:
+    #         Swap element i with element i // 2.
+    #         Set i to i // 2.
+    #     Else, do:
+    #         (all elements are already in the right place)
+    #
+    # Increment the size.
     pass
 
 
@@ -24,7 +43,28 @@ def dequeue(pqueue):
     #       level (the same very particular position to which we add leaves
     #       when enqueueing), so as to maintain both the structural integrity
     #       and the completeness of the tree. Then rearrange nodes as necessary
-    #       in order to maintain the heap property.
+    #       in order to maintain the heap property: repeatedly swap the current
+    #       node with its larger child if the current node is smaller.
+    #
+    # Start with index i being the index of the root (index 1).
+    # Set element i to the value of the last leaf (index size).
+    #
+    # While 2 * i is less than or equal to size (while i is not a leaf), do:
+    #     If index 2 * i + 1 is greater than size or element 2 * i is greater
+    #      than element 2 * i + 1, then:
+    #         If element 2 * i is greater than element i, then:
+    #             Swap element i with element 2 * i.
+    #             Set i to 2 * i.
+    #         Else, do:
+    #             (all elements are already in the right place)
+    #     Else, do:
+    #         If element 2 * i + 1 is greater than element i, then:
+    #             Swap element i with element 2 * i + 1.
+    #             Set i to 2 * i + 1.
+    #         Else, do:
+    #             (all elements are already in the right place)
+    #
+    # Decrement the size.
     pass
 
 
@@ -35,5 +75,5 @@ def peek(pqueue):
     #       knowing which one; the smallest must be a leaf, but there is no way
     #       of knowing which one).
     #
-    # (return the value at the root)
+    # Return element 1.
     pass
