3fb6370aa6e605f2193a5000437ef7afe5c82960
Christopher E. Siu <cesiu@calpoly.edu>
2025-02-24 16:10:48 -0800

    Added pseudocode for binary heaps.

--- 02.24-heaps/priority_queue.py
+++ 02.24-heaps/priority_queue.py
@@ -16,6 +16,24 @@ def enqueue(pqueue, value):
     #       leaf in a new lowest (if the old lowest was full) level, so as to
     #       maintain the completeness of the tree. Then rearrange nodes as
     #       necesssary in order to maintain the heap property.
+    #
+    # If the size is equal to the capacity, then:
+    #     Set the capacity to capacity * 2.
+    #     Create a new array of that capacity + 1.
+    #     (copy the elements from the old array into the new array)
+    #     Set the array to the new array.
+    #
+    # Start with index i being the index of the next leaf (index size + 1).
+    # Set element i to the given value.
+    #
+    # While i is greater than 1 (while i is not the root), do:
+    #     If element i is greater than element i // 2, then:
+    #         Swap element i with element i // 2.
+    #         Set i to i // 2.
+    #     Else, do:
+    #         (all elements are already in the right place)
+    #
+    # Increment the size.
     pass
 
 
@@ -25,6 +43,26 @@ def dequeue(pqueue):
     #       when enqueueing), so as to maintain both the structural integrity
     #       and the completeness of the tree. Then rearrange nodes as necessary
     #       in order to maintain the heap property.
+    #
+    # Start with index i being the index of the root (index 1).
+    # Set element i to the value of the last leaf (index size).
+    #
+    # While 2 * i is less than or equal to size (i is not a leaf), do:
+    #     If 2 * i + 1 is greater than size or element 2 * i is greater than
+    #      element 2 * i + 1, then:
+    #         If element 2 * i is greater than element i, then:
+    #             Swap element i with element 2 * i.
+    #             Set i to 2 * i.
+    #         Else, do:
+    #             (all elements are already in the right place).
+    #     Else, do:
+    #         If element 2 * i + 1 is greater than element i, then:
+    #             Swap element i with element 2 * i + 1.
+    #             Set i to 2 * i + 1.
+    #         Else, do:
+    #             (all elements are already in the right place).
+    #
+    # Decrement the size.
     pass
 
 
@@ -35,5 +73,5 @@ def peek(pqueue):
     #       knowing which one; the smallest must be a leaf, but there is no way
     #       of knowing which one).
     #
-    # (return the value at the root)
+    # Return element 1.
     pass
