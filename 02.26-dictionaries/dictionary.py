fc090b378a27dfce9e9d998007a44efefc195dcf
Christopher E. Siu <cesiu@calpoly.edu>
2025-02-26 12:06:55 -0800

    Added basic core logic for hash tables.

--- 02.26-dictionaries/dictionary.py
+++ 02.26-dictionaries/dictionary.py
@@ -11,14 +11,34 @@ class Dictionary:
 
 
 def get(dct, key):
+    # NOTE: Essentially, this is an array list in which we have to convert the
+    #       keys (of arbitrary type) into indices before we can access their
+    #       values within the array.
+    #
+    # Hash the given key and mod it by the capacity.
+    # Return the element at that hash code in the array.
     pass
 
 
 def insert(dct, key, value):
+    # TODO: This only works as long as there are no collisions. If two keys
+    #       were ever to hash to the same index, we would have no way of
+    #       knowing whether we were overwriting a different existing key.
+    #
+    # Hash the given key and mod it by the capacity.
+    #
+    # If the element at that hash code in the array is None, then:
+    #     Set the element at that hash code in the array to the given value.
+    #     Increment the size.
+    # Else, do:
+    #     Set the element at that hash code in the array to the given value.
     pass
 
 
 def remove(dct, key):
+    # Hash the given key and mod it by the capacity.
+    # Set the element at that hash code in the array to None.
+    # Decrement the size.
     pass
 
 
